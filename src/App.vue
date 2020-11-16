406af36b318c0cfadc948ff4b1b9c8bd750ab145
Guilherme-Melo <guilherme.melo@ibm.com>
2020-11-16 18:49:41 -0300

    Setting up the homepage component and delete the boiler plate one

--- src/App.vue
+++ src/App.vue
@@ -34,23 +34,9 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld />
+      <HomePage />
     </v-main>
   </v-app>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld";
-
-export default {
-  name: "App",
-
-  components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
-};
-</script>
+<script src="./App.js"></script>
